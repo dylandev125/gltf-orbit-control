b59de29e1ac3ad19e74922b709d4d725cb80e9e8
dylandev125 <pauleta0126@gmail.com>
2022-09-27 01:43:21 +0200

    orbit control

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import * as THREE from "three";
+import "./App.css";
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+// OBJLoader(THREE);
+
 
 function App() {
+  useEffect(() => {
+    
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+    var renderer = new THREE.WebGLRenderer({
+      canvas: document.querySelector("#bg"),
+    });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    camera.position.z = 7;
+    camera.position.y = 3;
+
+    const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
+    scene.add( directionalLight );
+    
+    const light = new THREE.AmbientLight( 0x404040 ); // soft white light
+    scene.add( light );
+
+    const controls = new OrbitControls( camera, document.querySelector("#bg") );
+    controls.maxDistance = 10;
+    controls.minDistance = 2;
+    controls.update();
+
+    const loader = new GLTFLoader();
+    loader.load(
+      'models/Mustang_69.gltf',
+      ( gltf ) => {
+        // called when the resource is loaded
+        scene.add( gltf.scene );
+      },
+      ( xhr ) => {
+        // called while loading is progressing
+        console.log( `${( xhr.loaded / xhr.total * 100 )}% loaded` );
+      },
+      ( error ) => {
+        // called when loading has errors
+        console.error( 'An error happened', error );
+      },
+    );
+
+    var animate = function () {
+      // controls.update();
+      requestAnimationFrame(animate);
+      renderer.render(scene, camera);
+    };
+    animate();
+  }, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <canvas id="bg"></canvas>
+      <main>
+      </main>
     </div>
   );
 }
---
+++ src/assetlist/index.js
@@ -0,0 +1,32 @@
+export const textureList = {
+    Bugatti: {
+        glass: {
+            'Glass Albeldo' : 'T_Bugatti_Chiron_Glass_Albedo.png',
+            'Glass AO' : 'T_Bugatti_Chiron_Glass_AO.png',
+            'Glass Height' : 'T_Bugatti_Chiron_Glass_Height.png',
+            'Glass Metalic' : 'T_Bugatti_Chiron_Glass_Metallic.png',
+            'Glass Normal' : 'T_Bugatti_Chiron_Glass_Normal.png',
+            'Glass Roughness' : 'T_Bugatti_Chiron_Glass_Roughness.png'
+        },
+        mainbody: {
+            'Albedo_01' : 'T_Bugatti_Chiron_MainBody_Albedo_01.png' ,
+            'Albedo_02' : 'T_Bugatti_Chiron_MainBody_Albedo_02.png' ,
+            'AO' : 'T_Bugatti_Chiron_MainBody_AO.png' ,
+            'Height' : 'T_Bugatti_Chiron_MainBody_Height.png' ,
+            'Metalic' : 'T_Bugatti_Chiron_MainBody_Metallic.png' ,
+            'Normal' : 'T_Bugatti_Chiron_MainBody_Normal.png' ,
+            'Roughness' : 'T_Bugatti_Chiron_MainBody_Roughness.png'
+            // 'Glass Albeldo' : 'T_Bugatti_Chiron_Glass_Albedo.png',
+            // 'Glass AO' : 'T_Bugatti_Chiron_Glass_AO.png',
+            // 'Glass Height' : 'T_Bugatti_Chiron_Glass_Height.png',
+            // 'Glass Metalic' : 'T_Bugatti_Chiron_Glass_Metallic.png',
+            // 'Glass Normal' : 'T_Bugatti_Chiron_Glass_Normal.png',
+            // 'Glass Roughness' : 'T_Bugatti_Chiron_Glass_Roughness.png'
+        },
+        
+    }
+}
+
+export const modelList = {
+    'Bugatti Chiron' : 'Bugatti',
+}
